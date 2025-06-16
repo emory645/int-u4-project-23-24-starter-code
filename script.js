4e4d0505ce03ef3d525558445ad11cca1f1e7102
emory645 <emorypp11@gmail.com>
2025-06-16 15:45:45 -0500

    quiz

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // JavaScript goes here.
+let position = document.querySelector(".Position")
+
+let groupOneButtons = document.querySelectorAll(".button-group-one")
+
+groupOneButtons.forEach(function(one) {
+    one.addEventListener ('click', function(){
+        position.style.display = "block";
+    })
+});
+
+
